36fcd4f045c5ec50664d3103dbd8d44e112f69f8
Nahi3an <nafisnahiyan321@gmail.com>
2022-03-21 13:19:09 +0600

    Custom validation added in Roles

---
+++ app/Rules/ValidateRole.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Roles;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidateRole implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return Roles::where('name', '=', $value)->count();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
