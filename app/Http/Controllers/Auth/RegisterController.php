bc730f835cc67a0e4770862eba75f34e90773a51
Nahi3an <nafisnahiyan321@gmail.com>
2022-03-24 04:48:04 +0600

    Country list generated on form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,6 +79,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        dd($data);
         User::create([
             'name' => $data['name'],
             'email' => $data['email'],
